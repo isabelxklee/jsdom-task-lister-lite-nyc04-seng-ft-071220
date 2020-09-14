25a6d3f895d3cbf6fdae75f1f2f8e76618e3724d
isabel k. lee <hello@isabelklee.co>
2020-09-14 12:26:53 -0400

    adding edit functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,65 @@
+let taskID = 1
+let userID = 1
+
 document.addEventListener("DOMContentLoaded", () => {
   let newTask = document.getElementById("new-task-description")
   let newUser = document.getElementById("new-user")
-  let taskForm = document.getElementById("create-task-form")
+  let editTask = document.getElementById("edit-task")
+  let editUser = document.getElementById("edit-user")
+  let newTaskForm = document.getElementById("create-task-form")
+  let editTaskForm = document.getElementById("edit-task-form")
   let unorderedList = document.getElementById("tasks")
   
-  taskForm.addEventListener("submit", (event) => {
+  newTaskForm.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    let taskItem = document.createElement("li")
-    taskItem.innerText = `"${newTask.value}" by ${newUser.value}`
+    let newTaskContainer = document.createElement("div")
+
+    let taskInfo = document.createElement("li")
+    taskInfo.innerText = newTask.value
+    taskInfo.id = `task-${taskID ++}`
+
+    let userInfo = document.createElement("li")
+    userInfo.innerText = newUser.value
+    userInfo.id = `user-${userID ++}`
 
     let deleteButton = document.createElement("button")
     deleteButton.innerText = "Delete"
 
+    let editButton = document.createElement("button")
+    editButton.innerText = "Edit"
+
     let lineBreak = document.createElement("br")
 
-    taskItem.append(lineBreak, deleteButton)
-    unorderedList.append(taskItem)
+    taskInfo.append(lineBreak)
+    newTaskContainer.append(taskInfo, userInfo, editButton, deleteButton)
+    unorderedList.append(newTaskContainer)
+
+    newTaskForm.reset()
 
     deleteButton.addEventListener("click", () => {
-      taskItem.remove()
+      newTaskContainer.remove()
+    })
+
+    editButton.addEventListener("click", () => {
+      editTask.value = taskInfo.innerText
+      editUser.value = userInfo.innerText
+
+      let currentTaskID = taskInfo.id
+      let currentUserID = userInfo.id
+      
+      console.log(currentTaskID)
+      console.log(currentUserID)
+
+      editTaskForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        console.log(currentTaskID)
+        console.log(currentUserID)
+        
+        // taskInfo.innerText = editTask.value
+        // userInfo.innerText = editUser.value
+      })
     })
   })
-
 })
