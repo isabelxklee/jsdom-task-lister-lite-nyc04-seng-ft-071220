f8c1830743b244984eba82b0168721907a7003e1
isabel k. lee <hello@isabelklee.co>
2020-09-14 10:10:39 -0400

    finished core deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let inputField = document.getElementById("new-task-description")
+  let taskForm = document.getElementById("create-task-form")
+  let list = document.getElementById("tasks")
+  
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let taskItem = document.createElement("li")
+    taskItem.innerText = inputField.value 
+    list.append(taskItem)
+  })
+
+
 });
